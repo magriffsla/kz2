cbb098b90f0526e826e927fe0cb1a96b3a5c83dd
владислав смаглюк <soho51@mail.ru>
2016-02-28 14:29:28 +0300

    mm

--- kz3/src/kz3/Controllers/HomeController.cs
+++ kz3/src/kz3/Controllers/HomeController.cs
@@ -1,28 +1,15 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Mvc;           
-using kz2.Models;
-using Arvelia.EF7.Sqlite.Repo;
+using Microsoft.AspNet.Mvc;
 
-namespace kz2.Controllers
+namespace kz3.Controllers
 {
     public class HomeController : Controller
     {
-        private StoreContext ctx;
-        private Repo<Subject> subjRepo;
-        public HomeController(StoreContext context)
-        {
-            ctx = context;
-            subjRepo = new Repo<Subject>(ctx);
-        }
         public IActionResult Index()
         {
-            if (ctx.Subjects.Count() ==0)
-            {
-                var subj = new Subject { };
-            }
             return View();
         }
 
